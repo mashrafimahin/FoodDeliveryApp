a2c34603d45ae8fdcec706deb6a38b161923d176
Mashrafi Mahin <mashrafimahin120@gmail.com>
2025-11-06 08:03:57 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // Enable code splitting
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Separate vendor chunks for better caching
-          vendor: ["react", "react-dom"],
-          router: ["react-router", "react-router-dom"],
-          ui: ["styled-components", "@fortawesome/react-fontawesome"],
-          firebase: ["firebase"],
-        },
-      },
-    },
-    // Enable minification
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true, // Remove console.log in production
-        drop_debugger: true, // Remove debugger statements
-      },
-    },
-    // Enable source maps for debugging (can be disabled for production)
-    sourcemap: false,
-    // Optimize chunk size
-    chunkSizeWarningLimit: 1000,
-  },
-  // Enable gzip compression
-  server: {
-    fs: {
-      // Allow serving files from one level up to the project root
-      allow: [".."],
-    },
-  },
-});
+})
